53b4a468e10033528e5254d09fb561e6f1f6e51f
greyfox209 <greyfox209@gmail.com>
2023-11-23 22:46:34 +0300

    Анимация хэдера по скроллу

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,8 @@
 const pageHeader = document.querySelector('.page__header');
 const headerToggle = document.querySelector('.header__toggle');
+
+// header toggle
+
 const headerNav = document.querySelector('.header__navigation');
 
 headerToggle.addEventListener('click', () => {
@@ -9,3 +12,52 @@ headerToggle.addEventListener('click', () => {
     headerNav.toggleAttribute('data-visible');
     pageHeader.toggleAttribute('data-overlay');
 });
+
+// hide header on scroll
+
+let lastScrollY = window.scrollY;
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+window.addEventListener('scroll', () => {
+  if (lastScrollY < window.scrollY) {
+    pageHeader.classList.add('page__header--hidden');
+  } else {
+    pageHeader.classList.remove('page__header--hidden');
+  }
+
+  if (!isElementInViewport(pageHeader)) {
+    headerToggle.classList.add('header__toggle--show');
+  } else {
+    headerToggle.classList.remove('header__toggle--show');
+  }
+
+  lastScrollY = window.scrollY;
+});
+
+
+
+
+/*
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  if (lastScrollY < window.scrollY) {
+    pageHeader.classList.add('page__header--hidden'),
+    headerToggle.classList.add('header__toggle--show');
+  } else {
+    pageHeader.classList.remove('page__header--hidden');
+  }
+
+  lastScrollY = window.scrollY;
+});
+*/
+
